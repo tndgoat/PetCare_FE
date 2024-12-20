b3ff57a51c29d6a529338093ded3910e5e89ccd9
Tung Nguyen <tungnd.goat@gmail.com>
2024-12-20 17:46:55 +0700

    add screens

---
+++ src/screens/Clinic/ClinicScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ClinicScreen = () => {
+  return (
+    <View>
+      <Text>ClinicScreen</Text>
+    </View>
+  )
+}
+
+export default ClinicScreen
---
+++ src/screens/Feeds/FeedsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FeedsScreen = () => {
+  return (
+    <View>
+      <Text>FeedsScreen</Text>
+    </View>
+  )
+}
+
+export default FeedsScreen
