b1e7b3a7d90a7018c033ce41fcfeb8a93828f2e9
Tung Nguyen <tungnd.goat@gmail.com>
2024-12-20 16:31:56 +0700

    update

--- src/navigation/AppMenuProvider.tsx
+++ src/navigation/AppMenuProvider.tsx
@@ -1,54 +1,51 @@
-import React from "react";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import OnboardingScreen from "../screens/Onboarding/OnboardingScreen";
-import HomeScreen from "../screens/Home/HomeScreen";
-import LogoOnboarding from "../screens/Onboarding/LogoOnboarding";
-import BottomBarNavigation from "./BottomBarNavigation";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Provider, useSelector } from "react-redux";
-import { RootState } from "../store/index";
-import { MenuProvider } from "react-native-popup-menu";
-import AllTransaction from "../screens/Home/AllTransaction";
-import LoginScreen from "../screens/Auth/LoginScreen";
-import RegisterScreen from "../screens/Auth/RegisterScreen";
-import AllBudget from "../screens/Home/AllBudget";
-import AllGoal from "../screens/Goal/AllGoal";
-const Stack = createStackNavigator();
+import React from 'react'
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import OnboardingScreen from '../screens/Onboarding/OnboardingScreen'
+import HomeScreen from '../screens/Home/HomeScreen'
+import LogoOnboarding from '../screens/Onboarding/LogoOnboarding'
+import BottomBarNavigation from './BottomBarNavigation'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Provider, useSelector } from 'react-redux'
+import { RootState } from '../store/index'
+import { MenuProvider } from 'react-native-popup-menu'
+import AllTransaction from '../screens/Home/AllTransaction'
+import LoginScreen from '../screens/Auth/LoginScreen'
+import RegisterScreen from '../screens/Auth/RegisterScreen'
+import AllBudget from '../screens/Home/AllBudget'
+import AllGoal from '../screens/Goal/AllGoal'
+const Stack = createStackNavigator()
 
 const AppMenuProvider = () => {
   // this is temp for testing
-  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null);
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = React.useState(null)
 
   React.useEffect(() => {
     const fetchData = async () => {
-      const appData = await AsyncStorage.getItem("isAppFirstLaunched");
+      const appData = await AsyncStorage.getItem('isAppFirstLaunched')
       if (appData == null) {
-        setIsAppFirstLaunched(true);
-        AsyncStorage.setItem("isAppFirstLaunched", "false");
+        setIsAppFirstLaunched(true)
+        AsyncStorage.setItem('isAppFirstLaunched', 'false')
       } else {
-        setIsAppFirstLaunched(false);
+        setIsAppFirstLaunched(false)
       }
-    };
+    }
 
-    fetchData();
+    fetchData()
     // AsyncStorage.removeItem('isAppFirstLaunched');
-  }, []);
+  }, [])
 
-  const isLogin = useSelector((state: RootState) => state.LoginStatus.isLogin);
+  const isLogin = useSelector((state: RootState) => state.LoginStatus.isLogin)
   const onBoarding = isAppFirstLaunched != null && (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isAppFirstLaunched && (
-          <>
-            <Stack.Screen name="LogoOnboarding" component={LogoOnboarding} />
-            <Stack.Screen
-              name="OnboardingScreen"
-              component={OnboardingScreen}
-            />
-          </>
-        )}
+        {/* {isAppFirstLaunched && ( */}
+        <>
+          <Stack.Screen name="LogoOnboarding" component={LogoOnboarding} />
+          <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+        </>
+        {/* )} */}
         {!isLogin && (
           <>
             <Stack.Screen name="LoginScreen" component={LoginScreen} />
@@ -58,7 +55,7 @@ const AppMenuProvider = () => {
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
   //   const loginScreen = (
   //     <NavigationContainer>
   //       <Stack.Navigator screenOptions={{ headerShown: true }}>
@@ -71,32 +68,20 @@ const AppMenuProvider = () => {
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={BottomBarNavigation} />
-          <Stack.Screen
-            name="AllTransaction"
-            component={AllTransaction}
-            options={{ headerShown: true }}
-          />
-          <Stack.Screen
-            name="All Budgets"
-            component={AllBudget}
-            options={{ headerShown: true }}
-          />
-          <Stack.Screen
-            name="All Goals"
-            component={AllGoal}
-            options={{ headerShown: true }}
-          />
+          <Stack.Screen name="AllTransaction" component={AllTransaction} options={{ headerShown: true }} />
+          <Stack.Screen name="All Budgets" component={AllBudget} options={{ headerShown: true }} />
+          <Stack.Screen name="All Goals" component={AllGoal} options={{ headerShown: true }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
-  );
+  )
 
   return (
     <MenuProvider>
       {/* {isAppFirstLaunched ? onBoarding : !isLogin ? loginScreen : mainScreen} */}
       {!isLogin ? onBoarding : mainScreen}
     </MenuProvider>
-  );
-};
+  )
+}
 
-export default AppMenuProvider;
+export default AppMenuProvider
--- src/screens/Onboarding/LogoOnboarding.tsx
+++ src/screens/Onboarding/LogoOnboarding.tsx
@@ -1,30 +1,22 @@
-import {
-  ImageBackground,
-  View,
-  Image,
-  Text,
-  SafeAreaView,
-  Dimensions,
-  TouchableOpacity,
-} from "react-native";
-import Logo from "../../images/lg.png";
-import Background from "../../images/bgg.png";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ImageBackground, View, Image, Text, SafeAreaView, Dimensions, TouchableOpacity } from 'react-native'
+import Logo from '../../images/logo.png'
+import Background from '../../images/bgg.png'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get('window')
 
 const LogoOnboarding = ({ navigation }: any) => {
   return (
     <TouchableOpacity
       style={{
         width: width,
-        overflow: "hidden",
+        overflow: 'hidden',
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
-      onPress={() => navigation.navigate("OnboardingScreen")}
+      onPress={() => navigation.navigate('OnboardingScreen')}
     >
       <ImageBackground source={Background}>
         <SafeAreaView
@@ -35,14 +27,14 @@ const LogoOnboarding = ({ navigation }: any) => {
         <Image
           source={Logo}
           style={{
-            maxWidth: "100%",
-            resizeMode: "contain",
+            maxWidth: '100%',
+            resizeMode: 'contain',
           }}
         />
         <SafeAreaView style={{ opacity: 0 }} />
       </ImageBackground>
     </TouchableOpacity>
-  );
-};
+  )
+}
 
-export default LogoOnboarding;
+export default LogoOnboarding
